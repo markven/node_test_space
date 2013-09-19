755cde5bf0d1f00d854c441068b9fcdf033ac1e3
gislab-ubuntu64 <markven@ubuntu.(none)>
2013-09-19 04:28:14 -0700

    20130919 first

---
+++ examples/basic/000-nonblock2.js
@@ -0,0 +1,12 @@
+setTimeout
+(
+	function()
+		{
+			console.log("bar...");
+		}	
+, 10000);
+
+//設定停留時間
+
+console.log("foo...");
+
---
+++ examples/basic/003-if.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+if ( process.argv[2] == 1 ) 
+	{
+		console.log('input 1...');
+	} 
+else if (process.argv[2] == 2) 
+	{
+		console.log('input 2...');
+	} 
+else 
+	{
+		console.log('not 1 or 2...');
+	}
+
